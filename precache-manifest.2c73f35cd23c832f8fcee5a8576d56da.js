890e200a20859256e8042e4b54e8004c83b4a9aa
Samuel Caçador <samuelmoreira27@gmail.com>
2019-07-12 20:53:37 +0100

    Updates

---
+++ precache-manifest.2c73f35cd23c832f8fcee5a8576d56da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab3af0b33ee4276f5d8f425722d75e9",
+    "url": "/countdown-doggo/index.html"
+  },
+  {
+    "revision": "317a393b4873b588a305",
+    "url": "/countdown-doggo/static/js/2.63d82b22.chunk.js"
+  },
+  {
+    "revision": "5a6cb92a5d1ffe11e6cb",
+    "url": "/countdown-doggo/static/js/main.83becbbb.chunk.js"
+  },
+  {
+    "revision": "a8c217ece20f68c2d859",
+    "url": "/countdown-doggo/static/js/runtime~main.bb1f20a0.js"
+  }
+]);
